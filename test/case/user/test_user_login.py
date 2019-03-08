430d1f6920d61ae9c493ba173e05c767a388d37a
gting <672654426@qq.com>
2019-03-08 10:02:57 +0800

    Initial commit

---
+++ test/case/user/test_user_login.py
@@ -0,0 +1,30 @@
+import unittest
+from test.case.basecase import BaseCase
+
+
+
+
+class TestUserLogin(BaseCase):
+    def test_user_login_normal(self):
+        """level1:正常登录"""
+        case_data = self.get_case_data("test_user_login_normal")
+        # print(case_data)
+        self.send_request(case_data)
+        print(self.send_request(case_data))
+    #
+    # def test_user_login_password_wrong(self):
+    #     """密码错误登录"""
+    #     case_data = self.get_case_data("test_user_login_password_wrong")
+    #     self.send_request(case_data)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
+    # t = TestUserLogin()
+    # print(t.test_user_login_normal().__tags__)
+    # suite = unittest.TestSuite()
+    # suite.addTest(TestUserLogin('test_user_login_normal'))
+    # for i in suite:
+    #     print(i)
+
+    # unittest.TextTestRunner().run(suite)
